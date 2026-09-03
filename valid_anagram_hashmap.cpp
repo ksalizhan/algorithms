eeab6771b230fe370404695c90ee3f39c621eb1e
ksalizhan <ksalizhan@gmail.com>
2026-09-03 17:53:11 +0100

    implemented valid_anagram via hashmap O(n) and O(n+m) times, space O(n)

---
+++ valid_anagram_hashmap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Time O(n)
+        // Space O(n) hashmap
+        unordered_map<char,int> seen;
+        if (s.length() != t.length()){
+            return false;
+        }
+        for (int i=0; i<s.length(); i++){
+            seen[s[i]]++;
+            seen[t[i]]--;
+        }
+        for (int j=0; j<t.length(); j++){
+            if(seen[s[j]]!= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
